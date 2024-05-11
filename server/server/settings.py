897363a9a9f47d8f29e8a2fbe2f5e10ae259c67c
shashmahawar <shshnkmhwr@gmail.com>
2024-05-12 00:26:03 +0530

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -98,10 +98,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = 'api/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
-MEDIA_URL = 'media/'
+MEDIA_URL = 'api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
