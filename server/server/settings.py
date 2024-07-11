905ea9aca68546ea49de8e76bdd7025c5490e2be
shashmahawar <shshnkmhwr@gmail.com>
2024-07-11 16:03:28 +0530

    Chagen ALLOWED_HOSTS

--- server/server/settings.py
+++ server/server/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['classgrid.devclub.iitd.tech']
+ALLOWED_HOSTS = ['classgrid-prod.devclub.iitd.tech', 'classgrid.devclub.in']
 
 INSTALLED_APPS = [
     # Default Django apps
