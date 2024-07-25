5bfa01c9ad625e41db4cfcbe7bc3f2104a75adf7
shashmahawar <shshnkmhwr@gmail.com>
2024-07-25 20:00:40 +0530

    Update update.py

--- server/app/utils/update.py
+++ server/app/utils/update.py
@@ -43,8 +43,8 @@ def check_course_update(last_course_update):
 
 def run():
     
-    curr_room_allotment = "https://timetable.iitd.ac.in/public/storage/uploads/schedule/Room_Allotment_Chart_2024_2025_1_1721668115.pdf"
-    last_course_update = "2024-07-22 14:02  "
+    curr_room_allotment = "https://timetable.iitd.ac.in/public/storage/uploads/schedule/Room_Allotment_Chart_2024_2025_1_1721841084.pdf"
+    last_course_update = "2024-07-25 13:33	"
 
     while True:
 
